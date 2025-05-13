40f70611f9c88aefe9720f4a4a0d41670279d38d
Iain Ireland <iireland@mozilla.com>
2025-05-13 16:19:43 -0700

    Initial three.js benchmark

--- JetStreamDriver.js
+++ JetStreamDriver.js
@@ -2011,6 +2011,15 @@ let BENCHMARKS = [
         ],
         tags: ["Generators"]
     }),
+    new DefaultBenchmark({
+        name: "threejs",
+        files: [
+            "./threejs/three.js",
+            "./threejs/benchmark.js",
+        ],
+        deterministicRandom: true,
+        tags: ["ThreeJs"],
+    }),
     // Wasm
     new WasmEMCCBenchmark({
         name: "HashSet-wasm",
