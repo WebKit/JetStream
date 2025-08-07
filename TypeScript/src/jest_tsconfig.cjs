899976ad0ba794c1781f64ab48b513727d504956
Camillo Bruni <cbruni@chromium.org>
2025-08-07 15:38:00 +0200

    add sources

---
+++ TypeScript/src/jest_tsconfig.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  "$schema": "https://json.schemastore.org/tsconfig",
+  "extends": "@tsconfig/node18/tsconfig.json",
+  "compilerOptions": {
+    "module": "preserve",
+    "moduleResolution": "bundler",
+    "composite": true,
+    "declaration": true,
+    "emitDeclarationOnly": true,
+    "stripInternal": true,
+    "strict": true,
+    "noUnusedLocals": true,
+    "noUnusedParameters": true,
+    "noImplicitOverride": true,
+    "noImplicitReturns": true,
+    "noFallthroughCasesInSwitch": true,
+    "esModuleInterop": false,
+    "isolatedModules": true,
+    "skipLibCheck": false,
+    "resolveJsonModule": true
+  }
+};
