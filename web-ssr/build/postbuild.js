dc87feeb0339ccc41faf7b59caf440ebf6f4cbc6
Camillo Bruni <cbruni@chromium.org>
2025-08-08 18:23:31 +0200

    update

--- web-ssr/build/postbuild.js
+++ web-ssr/build/postbuild.js
@@ -7,4 +7,9 @@ const srcOutPath = path.join(__dirname, "..", "dist", "react-render-test.src.js"
 const bundleContent = fs.readFileSync(bundlePath, "utf8");
 const srcContent = `REACT_RENDER_TEST_SRC = ${JSON.stringify(bundleContent)};`;
 fs.writeFileSync(srcOutPath, srcContent);
-console.log(`Successfully created ${srcOutPath}`);
+
+const stats = fs.statSync(srcOutPath);
+const fileSizeInKiB = (stats.size / 1024) | 0;  
+console.info(`Exported loadable src string`);
+console.info(`   File:       ${path.relative(process.cwd(), srcOutPath)}`);
+console.info(`   Total Size: ${fileSizeInKiB} KiB`);
