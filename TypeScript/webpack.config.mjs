ff3ecbe972956c2ac7f91e39c5b733cdc47dcfe8
Camillo Bruni <cbruni@chromium.org>
2025-08-20 18:09:46 +0200

    adding readme

---
+++ TypeScript/webpack.config.mjs
@@ -0,0 +1,39 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import TerserPlugin from "terser-webpack-plugin";
+import * as PathBrowserify from "path-browserify";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const commonConfig = {
+  mode: "production",
+  devtool: "source-map",
+  target: "web",
+  entry: path.resolve(__dirname, "src/test.cjs"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "typescript-compile-test.js",
+    library: {
+      name: "TypeScriptCompileTest",
+      type: "globalThis",
+    },
+    libraryTarget: "assign",
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          keep_fnames: true,
+        },
+      }),
+    ],
+  },
+  resolve: {
+    fallback: {
+      "path": "path-browserify",
+      "fs": false,
+    },
+  },
+};
+
+export default [commonConfig];
