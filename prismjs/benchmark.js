f89f472114001f2362697992f2b6954dde423df8
Camillo Bruni <cbruni@chromium.org>
2025-08-26 16:52:47 +0200

    format

--- prismjs/benchmark.js
+++ prismjs/benchmark.js
@@ -1,27 +1,27 @@
 /*
-* Copyright (C) 2025 Apple Inc. All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions
-* are met:
-* 1. Redistributions of source code must retain the above copyright
-*    notice, this list of conditions and the following disclaimer.
-* 2. Redistributions in binary form must reproduce the above copyright
-*    notice, this list of conditions and the following disclaimer in the
-*    documentation and/or other materials provided with the distribution.
-*
-* THIS SOFTWARE IS PROVIDED BY APPLE INC. ``AS IS'' AND ANY
-* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-* PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE INC. OR
-* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-* PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
-* OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ * Copyright (C) 2025 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 const EXPECTED_ASSERTION_COUNT = 1213680;
 
@@ -29,7 +29,7 @@ class Benchmark extends StartupBenchmark {
   lastResult;
   totalHash = 0xdeadbeef;
   samples = [];
-  
+
   constructor(iterationCount) {
     super({
       iterationCount,
@@ -65,9 +65,9 @@ class Benchmark extends StartupBenchmark {
   runIteration(iteration) {
     // Module is loaded into PrismJSBenchmark
     let PrismJSBenchmark;
-    eval(this.iterationSourceCodes[iteration])
+    eval(this.iterationSourceCodes[iteration]);
     this.lastResult = PrismJSBenchmark.runTest(this.samples);
-    
+
     for (const result of this.lastResult) {
       result.hash = this.quickHash(result.html);
       this.totalHash ^= result.hash;
@@ -80,7 +80,10 @@ class Benchmark extends StartupBenchmark {
       const sample = this.samples[i];
       const result = this.lastResult[i];
       console.assert(result.html.length > 0);
-      console.assert(result.hash == sample.hash, `Invalid result.hash = ${result.hash}, expected ${sample.hash}`);
+      console.assert(
+        result.hash == sample.hash,
+        `Invalid result.hash = ${result.hash}, expected ${sample.hash}`
+      );
     }
   }
 }
