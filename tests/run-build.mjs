b297335deb49d97062750e4f72db89fd37b02421
Camillo Bruni <cbruni@chromium.org>
2025-10-22 15:50:56 +0200

    more logging

--- tests/run-build.mjs
+++ tests/run-build.mjs
@@ -54,6 +54,8 @@ async function runBuilds() {
         return changedDirs.has(dir);
     });
     logInfo(`Found ${filteredPackageJsonFiles.length} package.json files to build`);
+    logInfo(process.env);
+    logInfo(filteredPackageJsonFiles);
 
     for (const file of filteredPackageJsonFiles) {
         const content = fs.readFileSync(file, "utf-8");
