f0af4c772e0ecf6775a628ba6c6c188860d0e6f7
Camillo Bruni <cbruni@chromium.org>
2026-02-09 11:59:37 +0100

    first-version

---
+++ tests/run-meta.mjs
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2025 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import { glob } from "glob";
+import * as fs from "fs";
+import { logInfo, logError } from "./helper.mjs";
+
+const IGNORE_PATTERNS = [
+    "**/node_modules/**",
+    "**/dist/**",
+    "**/build/**",
+    "**/.*/**",
+    // Existing benchmarks with common / non-standard license headers:
+    "**/worker/bomb-subtests/**",
+    "**/SunSpider/**",
+    "**/third_party/**"
+];
+
+async function checkLicenses() {
+    const files = await glob("**/*.{js,mjs}", { ignore: IGNORE_PATTERNS, nodir: true });
+    let failed = false;
+
+    logInfo(`Checking ${files.length} files for license headers...`);
+
+    const offendingFiles = [];
+
+    for (const file of files) {
+        const content = fs.readFileSync(file, "utf8");
+
+        const hasCopyright = /Copyright/i.test(content);
+        const hasLicenseText = /Redistribution|Permission|License/i.test(content);
+
+        if (!hasCopyright || !hasLicenseText) {
+            offendingFiles.push(file);
+            failed = true;
+        }
+    }
+
+    if (failed) {
+        logError("The following files are missing license headers:");
+        for (const file of offendingFiles) {
+             logError(`  ${file}`);
+        }
+        process.exit(1);
+    } else {
+        logInfo("All files have license headers.");
+    }
+}
+
+checkLicenses();
