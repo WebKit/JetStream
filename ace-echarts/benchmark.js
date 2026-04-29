74db58f8cbdc6a0862eec0b5ae583dda968b2182
Camillo Bruni <cbruni@chromium.org>
2026-04-29 16:36:47 +0200

    fx

--- ace-echarts/benchmark.js
+++ ace-echarts/benchmark.js
@@ -1,6 +1,6 @@
 class Benchmark {
     runIteration() {
-        var result = EChartsBenchmark.runTest();
+        const result = EChartsBenchmark.runTest();
         console.assert(result.options.length > 0);
     }
 }
